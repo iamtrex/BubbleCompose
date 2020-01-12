ce1f3c040cafa5c3a751c8d0ddbefd8f4a7a79b7
Ben Kwok <ben.kwok.cy@gmail.com>
2020-01-12 07:40:42 -0800

    Lower starting note

--- client/util.js
+++ client/util.js
@@ -34,7 +34,7 @@ function patternToMelody(client, pattern) {
 function convertYToPitch(y) {
     let nthNote = Math.round((100.0 - y) / 100.0 * NUM_NOTES); // Map to note linearly.
 
-    let pitch = 48;
+    let pitch = 34;
     for (let i = 0; i < nthNote; i++) {
         pitch += INTERVALS[i % INTERVALS.length];
     }
