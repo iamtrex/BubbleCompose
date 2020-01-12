6fae50b9309c5e8d5575f91e19da0e34e2b59296
Ben Kwok <ben.kwok.cy@gmail.com>
2020-01-12 00:23:05 -0800

    Map note numbers to freq

--- client/audio/audio.js
+++ client/audio/audio.js
@@ -9,7 +9,8 @@ let BPM = 60;
 // play note immediately and trigger draw circle
 function playNote(note) {
     console.log("Playing " + note.pitch);
-    piano.triggerAttackRelease(note.pitch);
+    let freq = noteToFreq(note.pitch);
+    piano.triggerAttackRelease(freq);
     Tone.Draw.schedule(() => {
         drawShape(note);
     }); 
@@ -22,7 +23,8 @@ function addMelody(melody) {
         for (let note of melody) {
             let pitch = note.pitch;
             console.log("Playing " + pitch);
-            piano.triggerAttackRelease(pitch, "8n", offset);
+            let freq = noteToFreq(note.pitch);
+            piano.triggerAttackRelease(freq, "8n", offset);
             Tone.Draw.schedule(() => {
                 drawShape(note);
             }); 
@@ -77,6 +79,11 @@ function getRandomIntInclusive(min, max) { // credit to MDN
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
   }
+
+function noteToFreq(noteNumber) {
+    let mapping = {1: 16.35, 2: 17.32, 3: 18.35, 4: 19.45, 5: 20.6, 6: 21.83, 7: 23.12, 8: 24.5, 9: 25.96, 10: 27.5, 11: 29.14, 12: 30.87, 13: 32.7, 14: 34.65, 15: 36.71, 16: 38.89, 17: 41.2, 18: 43.65, 19: 46.25, 20: 49.0, 21: 51.91, 22: 55.0, 23: 58.27, 24: 61.74, 25: 65.41, 26: 69.3, 27: 73.42, 28: 77.78, 29: 82.41, 30: 87.31, 31: 92.5, 32: 98.0, 33: 103.83, 34: 110.0, 35: 116.54, 36: 123.47, 37: 130.81, 38: 138.59, 39: 146.83, 40: 155.56, 41: 164.81, 42: 174.61, 43: 185.0, 44: 196.0, 45: 207.65, 46: 220.0, 47: 233.08, 48: 246.94, 49: 261.63, 50: 277.18, 51: 293.66, 52: 311.13, 53: 329.63, 54: 349.23, 55: 369.99, 56: 392.0, 57: 415.3, 58: 440.0, 59: 466.16, 60: 493.88, 61: 523.25, 62: 554.37, 63: 587.33, 64: 622.25, 65: 659.25, 66: 698.46, 67: 739.99, 68: 783.99, 69: 830.61, 70: 880.0, 71: 932.33, 72: 987.77, 73: 1046.5, 74: 1108.73, 75: 1174.66, 76: 1244.51, 77: 1318.51, 78: 1396.91, 79: 1479.98, 80: 1567.98, 81: 1661.22, 82: 1760.0, 83: 1864.66, 84: 1975.53, 85: 2093.0, 86: 2217.46, 87: 2349.32, 88: 2489.02, 89: 2637.02, 90: 2793.83, 91: 2959.96, 92: 3135.96, 93: 3322.44, 94: 3520.0, 95: 3729.31, 96: 3951.07, 97: 4186.01, 98: 4434.92, 99: 4698.63, 100: 4978.03, 101: 5274.04, 102: 5587.65, 103: 5919.91, 104: 6271.93, 105: 6644.88, 106: 7040.0, 107: 7458.62, 108: 7902.13};
+    return mapping[noteNumber];
+}
 
 // AT LOAD
 
